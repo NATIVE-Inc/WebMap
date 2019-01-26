6d8431ada0d41197b0dfb28d068ea7113cf7d126
realjema <realjema@gmail.com>
2019-01-26 06:53:25 +0100

    updates

--- script.js
+++ script.js
@@ -1057,11 +1057,11 @@ legend.onAdd = function (map) {
 function createCustomIcon (feature, latlng) {
     let myIcon = L.icon({
       iconUrl: 'img/eau_icon.png',
-      shadowUrl: 'img/eau_icon.png',
+    //   shadowUrl: 'img/eau_icon.png',
       iconSize:     [25, 25], // width and height of the image in pixels
-      shadowSize:   [35, 20], // width, height of optional shadow image
+    //   shadowSize:   [35, 20], // width, height of optional shadow image
       iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
-      shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
+    //   shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
       popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
     })
     return L.marker(latlng, { icon: myIcon })
